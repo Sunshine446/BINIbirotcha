e0d268c5f1b6c99642b1f23465847c6c8e5a1638
sunshine446 <sunshinenavarro5@gmail.com>
2024-06-20 15:33:45 +0800

    configure to database

--- src/main/java/com/sunshine/co/BINI/STORE/Controllers/UserController.java
+++ src/main/java/com/sunshine/co/BINI/STORE/Controllers/UserController.java
@@ -1,15 +1,40 @@
 package com.sunshine.co.BINI.STORE.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sunshine.co.BINI.STORE.Model.UserModel;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class UserController {
 
+    //Fetch One Data
+    //http://locathost.8080/users
     @GetMapping("/user")
     public UserModel getUserModel(){
        return new UserModel(1, "Jeven 11", "jeven.11@email.com", "jeven11");
     }
+
+    //Fetch Multiple Data
+    //http://localhost:8080/users
+    @GetMapping("/users")
+    public List<UserModel> getUsers(){
+        List<UserModel> users = new ArrayList<>();
+        users.add(new UserModel(1, "Rendo", "rendo@email.com", "rendo"));
+        users.add(new UserModel(2, "Rico", "rico@email.com", "rico"));
+        users.add(new UserModel(3, "Dudong", "dudong@email.com", "dudong"));
+        return users;
+    }
+    //http://localhost:8080/user/baby
+    @GetMapping("/user/{name}")
+    public UserModel getUserFromName(@PathVariable("name")String name){
+        return new UserModel(1, "Baby", "baby@email.com", "baby");
+    }
 }
+
